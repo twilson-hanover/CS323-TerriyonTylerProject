5e6853a2d291968ce7d5acf4f19b0e4ebb010b6c
walkert <walkert20@hanover.edu>
2018-05-23 11:53:30 -0400

    Created activity_theatre_price.xml file.

--- app/src/main/java/edu/hanover/cs323_terriyontylerproject/MainActivity.java
+++ app/src/main/java/edu/hanover/cs323_terriyontylerproject/MainActivity.java
@@ -2,6 +2,10 @@ package edu.hanover.cs323_terriyontylerproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> listView, View v, int position, long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(MainActivity.this, Theatre.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        ListView listView = (ListView)findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
---
+++ app/src/main/java/edu/hanover/cs323_terriyontylerproject/TheatrePrice.java
@@ -0,0 +1,16 @@
+package edu.hanover.cs323_terriyontylerproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class TheatrePrice extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theatre_price);
+
+    }
+}
