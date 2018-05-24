941e419cf7d687747c4e17292432dd5ada4575df
walkert <walkert20@hanover.edu>
2018-05-24 14:04:15 -0400

    Mad a couple of commits.

--- app/src/main/java/edu/hanover/cs323_terriyontylerproject/MainActivity.java
+++ app/src/main/java/edu/hanover/cs323_terriyontylerproject/MainActivity.java
@@ -3,11 +3,13 @@ package edu.hanover.cs323_terriyontylerproject;
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +29,16 @@ public class MainActivity extends Activity {
         };
         ListView listView = (ListView)findViewById(R.id.list_options);
         listView.setOnItemClickListener(itemClickListener);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+//    private void setActionBarTitle(int position) {
+//        String title;
+//        if (position == 0){
+//            title = getResources().getString(R.string.app_name);
+//        } else {
+//            title = titles[position];
+//        }
+//        getActionBar().setTitle(title);
+//    }
 }
