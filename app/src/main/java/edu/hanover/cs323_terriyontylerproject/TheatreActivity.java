14d761183455a1710cc5b53467c80ffdc08c947b
walkert <walkert20@hanover.edu>
2018-05-24 16:31:36 -0400

    More Edits.

--- app/src/main/java/edu/hanover/cs323_terriyontylerproject/TheatreActivity.java
+++ app/src/main/java/edu/hanover/cs323_terriyontylerproject/TheatreActivity.java
@@ -1,7 +1,9 @@
 package edu.hanover.cs323_terriyontylerproject;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -13,15 +15,17 @@ public class TheatreActivity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_theatre);
         ListView listTheatres = getListView();
 
         ArrayAdapter<Theatre> listAdapter = new ArrayAdapter<Theatre>(
                 this, android.R.layout.simple_list_item_1,
                 Theatre.theatres);
         listTheatres.setAdapter(listAdapter);
+
     }
 
-    @Override
+
     //create onListItemClick
     public void onListItemClick(ListView listView, View itemView, int position, long id) {
         Intent intent = new Intent(TheatreActivity.this, TheatrePrice.class);
